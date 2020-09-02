d1838e379e689ec160b46a1fec0342e52327673a
bielasilva <gabrielamorimsilva@gmail.com>
2020-09-01 23:02:06 -0300

    RefSeq random download working correctly but categories numbers not set

--- mock_ncbi_down.py
+++ mock_ncbi_down.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python3
 
-import csv, random, os
-random.seed(5)
+import csv, random, os, argparse
+from pathlib import Path
 
-in_file = "/home/gabrielasilva/Documents/projetos/mock_ncbi_down/assembly_summary_fungi.txt"
-down_directory = "/home/gabrielasilva/Documents/projetos/mock_ncbi_down/download"
-out_file = "/home/gabrielasilva/Documents/projetos/mock_ncbi_down/mock_downloaded.tsv"
+random.seed(1)
 
-def random_refseq (seq_number, category):
-    # Download Assembly summary
-    os.mkdir(f"{down_directory}/{category}")
-    ftp_link = "ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/"
-    os.system(f"wget -O assembly_summary{category}.txt -P {down_directory}/{category} -c {ftp_link} ")
+# Argumentos de entrada
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-d","--directory_download", type=str, help="Diretório destido do download", required=True)
+parser.add_argument("-c", "--categories", type=str, nargs='+', choices=["archaea", "bacteria", "fungi", "protozoa", "viral"], help="Categorias", required=True)
+
+args = vars(parser.parse_args())
+
+download_directory = str(Path(args["directory_download"]).resolve())
+categories = args["categories"]
+
+# in_file = "/home/gabrielasilva/Documents/projetos/mock_ncbi_down/assembly_summary_fungi.txt"
+# download_directory = "/home/gabrielasilva/Documents/projetos/mock_ncbi_down/download"
+# out_file = "/home/gabrielasilva/Documents/projetos/mock_ncbi_down/mock_downloaded.tsv"
+
+def random_refseq (seq_number, database, category):
+    # Download Assembly_summary
+    try:
+        os.mkdir(f"{download_directory}/{category}")
+    except OSError as error:
+        print(error)
+    ftp_link = f"ftp://ftp.ncbi.nlm.nih.gov/genomes/{database}/{category}"
+    os.system(f"wget -O {download_directory}/{category}/assembly_summary_{category}.txt -c {ftp_link}/assembly_summary.txt")
+    in_file = f"{download_directory}/{category}/assembly_summary_{category}.txt"
+    # Download Genomas
+    out_file = f"{download_directory}/mock_downloaded.tsv"
     with open(in_file) as i_file, open(out_file,"w+") as o_file:
         csv_read = csv.reader(i_file, delimiter="\t")
         next(csv_read)
@@ -21,16 +40,30 @@ def random_refseq (seq_number, category):
                 complete_entries.append(row)
         rand_choice = random.choices(complete_entries, k=seq_number)
         downloaded = []
-        header = ["assembly_accession", "organism_name", "infraspecific_name", "ftp_path"]
+        header = ["category", "assembly_accession", "organism_name", "infraspecific_name", "ftp_path"]
         csv_write = csv.DictWriter(o_file, fieldnames=header, delimiter="\t")
         csv_write.writeheader()
         for item in rand_choice:
             csv_write.writerow({
+                "category": category,
                 "assembly_accession": item[0],
                 "organism_name": item[7],
                 "infraspecific_name": item[8],
                 "ftp_path": item[19],
             })
             genome_link = item[19] + "/" + item[19].split("/")[-1] + "_genomic.fna.gz"
-            os.system(f"wget -P {down_directory} -c {genome_link} ")
+            os.system(f"wget -P {download_directory}/{category} -c {genome_link} ")
 
+for cat in categories:
+    if cat == "archaea":
+        random_refseq(2, "refseq", cat)
+    elif cat == "bacteria":
+        random_refseq(2, "refseq", cat)
+    elif cat == "fungi":
+        random_refseq(2, "refseq", cat)
+    elif cat == "protozoa":
+        random_refseq(2, "refseq", cat)
+    elif cat == "viral":
+        random_refseq(2, "refseq", cat)
+    else:
+        print(f"{cat} Categoria inválida")
