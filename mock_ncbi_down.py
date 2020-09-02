0e59a943bc28dd961c42f48dd43c627e4cfe8beb
bielasilva <gabrielamorimsilva@gmail.com>
2020-09-01 23:53:07 -0300

    RefSeq directory creation and small fixes

--- mock_ncbi_down.py
+++ mock_ncbi_down.py
@@ -5,33 +5,34 @@ from pathlib import Path
 
 random.seed(1)
 
-# Argumentos de entrada
+# Entry arguments
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-d","--directory_download", type=str, help="Diretório destido do download", required=True)
-parser.add_argument("-c", "--categories", type=str, nargs='+', choices=["archaea", "bacteria", "fungi", "protozoa", "viral"], help="Categorias", required=True)
+parser.add_argument("-d","--directory_download", type=str, help="Directory to save downloads", required=True)
+parser.add_argument("-c", "--databases", type=str, nargs='+', choices=["archaea", "bacteria", "fungi", "protozoa", "viral"], help="Databases", required=True)
 
 args = vars(parser.parse_args())
 
 download_directory = str(Path(args["directory_download"]).resolve())
-categories = args["categories"]
+databases = sorted(args["databases"])
 
 # in_file = "/home/gabrielasilva/Documents/projetos/mock_ncbi_down/assembly_summary_fungi.txt"
 # download_directory = "/home/gabrielasilva/Documents/projetos/mock_ncbi_down/download"
 # out_file = "/home/gabrielasilva/Documents/projetos/mock_ncbi_down/mock_downloaded.tsv"
+first = True
 
-def random_refseq (seq_number, database, category):
-    # Download Assembly_summary
+def random_refseq (seq_number, database):
+    # Assembly_summary Download
     try:
-        os.mkdir(f"{download_directory}/{category}")
-    except OSError as error:
-        print(error)
-    ftp_link = f"ftp://ftp.ncbi.nlm.nih.gov/genomes/{database}/{category}"
-    os.system(f"wget -O {download_directory}/{category}/assembly_summary_{category}.txt -c {ftp_link}/assembly_summary.txt")
-    in_file = f"{download_directory}/{category}/assembly_summary_{category}.txt"
-    # Download Genomas
+        os.makedirs(f"{download_directory}/{database}/refseq")
+    except FileExistsError:
+        pass
+    ftp_link = f"ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/{database}"
+    os.system(f"wget -O {download_directory}/{database}/refseq/assembly_summary_refseq_{database}.txt -c {ftp_link}/assembly_summary.txt")
+    in_file = f"{download_directory}/{database}/refseq/assembly_summary_refseq_{database}.txt"
+    # Genomes Download
     out_file = f"{download_directory}/mock_downloaded.tsv"
-    with open(in_file) as i_file, open(out_file,"w+") as o_file:
+    with open(in_file) as i_file, open(out_file,"a+") as o_file:
         csv_read = csv.reader(i_file, delimiter="\t")
         next(csv_read)
         complete_entries = []
@@ -42,28 +43,37 @@ def random_refseq (seq_number, database, category):
         downloaded = []
         header = ["category", "assembly_accession", "organism_name", "infraspecific_name", "ftp_path"]
         csv_write = csv.DictWriter(o_file, fieldnames=header, delimiter="\t")
-        csv_write.writeheader()
+        global first
+        if first:
+            csv_write.writeheader()
+            first = False
         for item in rand_choice:
             csv_write.writerow({
-                "category": category,
+                "category": database,
                 "assembly_accession": item[0],
                 "organism_name": item[7],
                 "infraspecific_name": item[8],
                 "ftp_path": item[19],
             })
             genome_link = item[19] + "/" + item[19].split("/")[-1] + "_genomic.fna.gz"
-            os.system(f"wget -P {download_directory}/{category} -c {genome_link} ")
+            os.system(f"wget -P {download_directory}/{database}/refseq/ -c {genome_link} ")
 
-for cat in categories:
-    if cat == "archaea":
-        random_refseq(2, "refseq", cat)
-    elif cat == "bacteria":
-        random_refseq(2, "refseq", cat)
-    elif cat == "fungi":
-        random_refseq(2, "refseq", cat)
-    elif cat == "protozoa":
-        random_refseq(2, "refseq", cat)
-    elif cat == "viral":
-        random_refseq(2, "refseq", cat)
+def genbank_download(seq_number, database, category):
+    try:
+        os.makedirs(f"{download_directory}/{category}/genbank")
+    except FileExistsError:
+        pass
+    
+for db in databases:
+    if db == "archaea":
+        random_refseq(2, db)
+    elif db == "bacteria":
+        random_refseq(2, db)
+    elif db == "fungi":
+        random_refseq(2, db)
+    elif db == "protozoa":
+        random_refseq(2, db)
+    elif db == "viral":
+        random_refseq(2, db)
     else:
-        print(f"{cat} Categoria inválida")
+        print(f"{db} Categoria inválida")
